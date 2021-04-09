9cc7efba74ee07fbc27013f000d5e817546fc2d1
33646341 <1759220040@qq.com>
2021-04-09 17:56:59 +0800

    一个项目草稿 (演示使用 HandyControl.3.1.0)

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Window1 w1 = new Window1();
+            //w1.Show();
+
+            NavigationWindow window = new NavigationWindow();
+            window.Source = new Uri("Page1.xaml", UriKind.Relative);
+            window.Show();
+            //window.content = new Page1();
+
+            //NavigationWindow window = new NavigationWindow();
+            //window.Source = new Uri("Page1.xaml", UriKind.Relative);
+            //window.Show();
+        }
+
+        private void mycheck_Copy_Checked(object sender, RoutedEventArgs e)
+        {
+            lbl.Content = "已选择";
+        }
+
+        private void mycheck_Copy_Unchecked(object sender, RoutedEventArgs e)
+        {
+            lbl.Content = "已取消";
+        }
+    }
+}
---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
