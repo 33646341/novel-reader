b367e2eebfd04e681e4115445db0fb663126a943
33646341 <1759220040@qq.com>
2021-04-17 10:20:30 +0800

    解压自 小说爬虫.zip

---
+++ Novel_Spider/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Novel_Spider
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Url_Txt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 272);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Url_Txt
+            // 
+            this.Url_Txt.Location = new System.Drawing.Point(225, 201);
+            this.Url_Txt.Name = "Url_Txt";
+            this.Url_Txt.Size = new System.Drawing.Size(344, 25);
+            this.Url_Txt.TabIndex = 1;
+            this.Url_Txt.Text = "https://www.biquzhh.com/29719_29719087/";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "请输入小说网址（仅限笔趣阁）：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Url_Txt);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Url_Txt;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
