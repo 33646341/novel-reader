51fe842b8875d230c4cc5c97869aa3eff979d244
自然 非常 <3089545054@qq.com>
2021-05-23 16:53:24 +0800

    使用HandyControl控件，做出主页面基本框架。下一步做更加细节的数据绑定。

--- UIdesign/MainWindow.xaml.cs
+++ UIdesign/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace UIdesign
         {
             InitializeComponent();
         }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
