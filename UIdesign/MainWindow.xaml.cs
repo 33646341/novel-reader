7f3738267f2995ac40ec62f5d9a1794ae79f218a
33646341 <1759220040@qq.com>
2021-06-05 23:04:03 +0800

    小修正

--- UIdesign/MainWindow.xaml.cs
+++ UIdesign/MainWindow.xaml.cs
@@ -24,9 +24,9 @@ namespace UIdesign
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window //INotifyPropertyChanged
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        #region
+        #region 加载圈相关
         public event PropertyChangedEventHandler PropertyChanged;
         private Visibility showProgress = Visibility.Collapsed;
         public Visibility ShowProgress
@@ -35,7 +35,7 @@ namespace UIdesign
             set
             {
                 showProgress = value;
-                //PropertyChanged(this, new PropertyChangedEventArgs(nameof(ShowProgress)));
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(ShowProgress)));
             }
         }
         #endregion
@@ -68,7 +68,7 @@ namespace UIdesign
             String kw = keySearch.Text;
             form.te = kw;
 
-            //ShowProgress = Visibility.Visible;
+            ShowProgress = Visibility.Visible;
 
             List<fiction_info> _ltfi_Search = new List<fiction_info>();
             List<Student> stuList = new List<Student>()
@@ -82,25 +82,25 @@ namespace UIdesign
             };
             this.Lv_HomePage.ItemsSource = stuList;//数据源
 
-            /*new Thread(() =>
+            new Thread(() =>
             {
                 for (int i = 10000; i < 10020; i++)
                 {
-                    var fiction_i = new fiction_info();
-                    fiction_i.col_fiction_name = i.ToString();
+                    var fiction_i = new Student() { Id = 5, Name = "Mike", Age = 22 };
+                    fiction_i.Id = i;
                     Thread.Sleep(200);
 
                     Dispatcher.Invoke(delegate ()
                     {
-                        _ltfi_Search.Add(fiction_i);
+                        stuList.Add(fiction_i);
                         Lv_HomePage.ItemsSource = null;
-                        Lv_HomePage.ItemsSource = _ltfi_Search;//刷新数据源
+                        Lv_HomePage.ItemsSource = stuList;//刷新数据源
                     });
                     //Lv_HomePage.ItemsSource = _ltfi_Search;//数据源
                     //PropertyChanged(this, new PropertyChangedEventArgs(nameof(_ltfi_Search)));
                 }
                 ShowProgress = Visibility.Collapsed;
-            }).Start();*/
+            }).Start();
 
             //List<fiction_info> _ltfi_Search = form.button1_Click();//_ltfi_Search是列表，每一项是一个小说信息
 
@@ -116,10 +116,10 @@ namespace UIdesign
         private void SListView_ItemDoubleClick(object sender, RoutedEventArgs e)
         {
             Student emp = Lv_HomePage.SelectedItem as Student;
-            Window1 login1 = new Window1(emp.Name,emp.Id,emp.Age);   //Login为窗口名，把要跳转的新窗口实例化
+            Window1 login1 = new Window1(emp.Name, emp.Id, emp.Age);   //Login为窗口名，把要跳转的新窗口实例化
             login1.Show();
         }   //打开新窗口
-            
+
     }
     public class Student
     {
