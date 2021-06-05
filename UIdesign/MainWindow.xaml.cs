f5e2f4b312c64efe70dcd0bfd5f459af3ef7b3d8
自然 非常 <3089545054@qq.com>
2021-06-05 22:16:31 +0800

    完成listview的演示，等待爬虫功能；连接主页和详情页。

--- UIdesign/MainWindow.xaml.cs
+++ UIdesign/MainWindow.xaml.cs
@@ -24,54 +24,65 @@ namespace UIdesign
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window //INotifyPropertyChanged
     {
+        #region
         public event PropertyChangedEventHandler PropertyChanged;
-
         private Visibility showProgress = Visibility.Collapsed;
-
         public Visibility ShowProgress
         {
             get { return showProgress; }
             set
             {
                 showProgress = value;
-                PropertyChanged(this, new PropertyChangedEventArgs(nameof(ShowProgress)));
+                //PropertyChanged(this, new PropertyChangedEventArgs(nameof(ShowProgress)));
             }
         }
-
+        #endregion
         public MainWindow()
         {
             InitializeComponent();
+            #region
             DataContext = this;
+            #endregion
         }
-
+        #region
         private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
-
         private void textBoxId_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
-
-
-
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+        #endregion
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             OnlineSearchAndRead.Form1 form = new OnlineSearchAndRead.Form1();
             String kw = keySearch.Text;
             form.te = kw;
 
-            ShowProgress = Visibility.Visible;
+            //ShowProgress = Visibility.Visible;
 
             List<fiction_info> _ltfi_Search = new List<fiction_info>();
+            List<Student> stuList = new List<Student>()
+            {
+                new Student() { Id = 0, Name = "Tim", Age = 29 },
+                new Student() { Id = 1, Name = "Tom", Age = 28 },
+                new Student() { Id = 2, Name = "Kyle", Age = 27 },
+                new Student() { Id = 3, Name = "Tony", Age = 24 },
+                new Student() { Id = 4, Name = "Vina", Age = 26 },
+                new Student() { Id = 5, Name = "Mike", Age = 22 },
+            };
+            this.Lv_HomePage.ItemsSource = stuList;//数据源
 
-            this.listBoxStudents.ItemsSource = _ltfi_Search;//数据源
-            this.listBoxStudents.DisplayMemberPath = "col_fiction_name";//路径
-
-            new Thread(() =>
+            /*new Thread(() =>
             {
                 for (int i = 10000; i < 10020; i++)
                 {
@@ -82,43 +93,37 @@ namespace UIdesign
                     Dispatcher.Invoke(delegate ()
                     {
                         _ltfi_Search.Add(fiction_i);
-                        listBoxStudents.ItemsSource = null;
-                        listBoxStudents.ItemsSource = _ltfi_Search;//刷新数据源
+                        Lv_HomePage.ItemsSource = null;
+                        Lv_HomePage.ItemsSource = _ltfi_Search;//刷新数据源
                     });
-                    //listBoxStudents.ItemsSource = _ltfi_Search;//数据源
+                    //Lv_HomePage.ItemsSource = _ltfi_Search;//数据源
                     //PropertyChanged(this, new PropertyChangedEventArgs(nameof(_ltfi_Search)));
                 }
                 ShowProgress = Visibility.Collapsed;
-            }).Start();
+            }).Start();*/
+
             //List<fiction_info> _ltfi_Search = form.button1_Click();//_ltfi_Search是列表，每一项是一个小说信息
 
             //为ListView设置Binding
 
             //ListView.ItemsSourceProperty.
             //为TextBox设置Binding
-            //Binding binding = new Binding("SelectedItem.col_fiction_name") { Source = this.listBoxStudents };
+            //Binding binding = new Binding("SelectedItem.col_fiction_name") { Source = this.Lv_HomePage };
             //this.textBoxId.SetBinding(TextBox.TextProperty, binding);
 
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void SListView_ItemDoubleClick(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void listBoxStudents_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-        private void SListBox_ItemDoubleClick(object sender, RoutedEventArgs e)
-        {
-            Window1 login1 = new Window1();   //Login为窗口名，把要跳转的新窗口实例化
-            login1.Show();   //打开新窗口
-            //this.Close();
-        }
+            Student emp = Lv_HomePage.SelectedItem as Student;
+            Window1 login1 = new Window1(emp.Name,emp.Id,emp.Age);   //Login为窗口名，把要跳转的新窗口实例化
+            login1.Show();
+        }   //打开新窗口
+            
     }
     public class Student
     {
+
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
--- UIdesign/Window1.xaml.cs
+++ UIdesign/Window1.xaml.cs
@@ -19,14 +19,18 @@ namespace UIdesign
     /// </summary>
     public partial class Window1 : Window
     {
-        public Window1()
+        public Window1(string fiction_name,int author_name,int total_number)
         {
             InitializeComponent();
+            Fiction_name.Text = fiction_name;
+            //Author_name.Text = Convert.ToString(author_name);
+            Total_number.Text = $"小说|完结|{total_number}字";
+
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
@@ -35,6 +39,16 @@ namespace UIdesign
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
         {
 
         }
