687cf87378cff1aeb6e0ed7be58fece1d5bf9c66
yuu <1269943206@qq.com>
2021-06-06 15:17:24 +0800

    添加阅读页与详情页 写有示例list

--- UIdesign/Window1.xaml.cs
+++ UIdesign/Window1.xaml.cs
@@ -19,19 +19,49 @@ namespace UIdesign
     /// </summary>
     public partial class Window1 : Window
     {
+        List<topic> topiclist = new List<topic>()
+            {
+                new topic(){number = 1,name="第一章名",content="内容1" },
+            new topic() { number = 2, name = "第二章名", content = "内容2" },
+            new topic() { number = 3, name = "第三章名", content = "内容3" },
+            new topic() { number = 4, name = "第四章名", content = "内容4" },
+            new topic() { number = 5, name = "第五章名", content = "内容5" }
+            };
         public Window1(string fiction_name,int author_name,int total_number)
         {
             InitializeComponent();
             Fiction_name.Text = fiction_name;
             //Author_name.Text = Convert.ToString(author_name);
             Total_number.Text = $"小说|完结|{total_number}字";
-
+            //List<topic> topiclist = new List<topic>()
+            //{
+            //    new topic(){number = 1,name="第一章名",content="内容1" },
+            //new topic() { number = 2, name = "第二章名", content = "内容2" },
+            //new topic() { number = 3, name = "第三章名", content = "内容3" },
+            //new topic() { number = 4, name = "第四章名", content = "内容4" },
+            //new topic() { number = 5, name = "第五章名", content = "内容5" }
+            //};
+            this.detaillist.ItemsSource = topiclist;
         }
 
+        #region 立即阅读
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            
+            var firstint = topiclist.First();
+            ReadWindow readWindow1 = new ReadWindow(firstint.number,firstint.name,firstint.content);
+            readWindow1.Show();
+            this.Close();
         }
+        #endregion
+
+        #region 双击item阅读
+        private void SListView_ItemDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            topic emp = (sender as ListViewItem).Content as topic;
+            ReadWindow readWindow1 = new ReadWindow(emp.number,emp.name,emp.content);
+            readWindow1.Show();
+        }
+        #endregion
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
@@ -53,4 +83,14 @@ namespace UIdesign
 
         }
     }
+
+
+    #region 数据源
+    public class topic
+    {
+        public int number { get; set; }
+        public string name { get; set; }
+        public string content { get; set; }
+    }
+    #endregion
 }
