3527d78a3e62f3e9bb73d847edec0588c0679df3
2408584819@qq.com <2408584819@qq.com>
2021-06-11 14:50:54 +0800

    适应前端

--- Novel_Spider/Form1.cs
+++ Novel_Spider/Form1.cs
@@ -104,10 +104,10 @@ namespace Novel_Spider
                             chapter_num[0]++;
 
                             download_progress = 1.0 * chapter_num[0] / chapter_sum[0] * 100;
-                            MethodInvoker m = new MethodInvoker(() =>
-                            {
-                                progressBar1.Value = Convert.ToInt32(download_progress);
-                            });
+                           // MethodInvoker m = new MethodInvoker(() =>
+                            //{
+                               // progressBar1.Value = Convert.ToInt32(download_progress);
+                            //});
                             //this.BeginInvoke(m);
 
                             if (chapter_sum[0] - 1 == chapter_num[0])
@@ -116,15 +116,15 @@ namespace Novel_Spider
                                 book.RemoveAt(0);
                                 chapter_num.RemoveAt(0);
                                 chapter_sum.RemoveAt(0);
-                                MethodInvoker mi = new MethodInvoker(() =>
-                                {
+                                //MethodInvoker mi = new MethodInvoker(() =>
+                               //{
                                     download_progress = 100;
 
-                                    listBox1.Items.RemoveAt(0);
+                                    //listBox1.Items.RemoveAt(0);
 
                                     if(chapter_sum.Count != 0)
-                                        progressBar1.Value = 0;
-                                });
+                                        download_progress = 0;
+                                //});
                                 //this.BeginInvoke(mi);
                             }
                         }
@@ -132,13 +132,13 @@ namespace Novel_Spider
                     }
                     if (tag == true)
                     {
-                        MethodInvoker mi = new MethodInvoker(() =>
-                        {
+                        //MethodInvoker mi = new MethodInvoker(() =>
+                        //{
                             download_progress = 100;
-                            progressBar1.Value = 100;
-                            MessageBox.Show("确定");
-                            this.Close();
-                        });
+                            //progressBar1.Value = 100;
+                            //MessageBox.Show("确定");
+                            //this.Close();
+                       // });
                         //this.BeginInvoke(mi);
                     }
                 });
@@ -163,10 +163,10 @@ namespace Novel_Spider
                             chapter_num[0]++;
 
                             download_progress = 1.0 * chapter_num[0] / chapter_sum[0] * 100;
-                            MethodInvoker m = new MethodInvoker(() =>
-                            {
-                                progressBar1.Value = Convert.ToInt32(download_progress);
-                            });
+                           // MethodInvoker m = new MethodInvoker(() =>
+                           // {
+                               // progressBar1.Value = Convert.ToInt32(download_progress);
+                            //});
                             //this.BeginInvoke(m);
 
                             if (chapter_sum[0] == chapter_num[0])
@@ -175,15 +175,13 @@ namespace Novel_Spider
                                 book.RemoveAt(0);
                                 chapter_num.RemoveAt(0);
                                 chapter_sum.RemoveAt(0);
-                                MethodInvoker mi = new MethodInvoker(() =>
-                                {
+                             
                                     download_progress = 100;
 
                                     listBox1.Items.RemoveAt(0);
 
                                     if (chapter_sum.Count != 0)
                                         progressBar1.Value = 0;
-                                });
                                 //this.BeginInvoke(mi);
                             }
                         }
