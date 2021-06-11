038bf9cd41f2729f36f133fed1aae9b33c541a80
2408584819@qq.com <2408584819@qq.com>
2021-06-11 20:08:35 +0800

    添加了chapter_comment类，保存评论和得到评论

---
+++ ReadTool/ReadTool/chapter_comment.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadTool
+{
+    class chapter_comment
+    {
+        public void save_comment(string novel_name, string chapter_name, string comment) //保存评论到文件，需要给出小说名称、章节名称、评论
+        {
+            if (!Directory.Exists(System.AppDomain.CurrentDomain.BaseDirectory + "/History/" + novel_name))
+                Directory.CreateDirectory(System.AppDomain.CurrentDomain.BaseDirectory + "/History/" + novel_name);//创建小说名文件夹
+            string chapter_path = System.AppDomain.CurrentDomain.BaseDirectory + "/History/" + novel_name + "/" + chapter_name + ".txt";
+            using (FileStream fs = new FileStream(chapter_path, FileMode.Append, FileAccess.Write))
+            {
+                fs.Lock(0, fs.Length);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine(comment);
+                fs.Unlock(0, fs.Length);//一定要用在Flush()方法以前，否则抛出异常。
+                sw.Flush();
+            }
+        }
+
+        public string load_comment(string novel_name, string chapter_name)//得到评论，需要给出小说名称、章节名称
+        {
+            string chapter_comment = "";
+            if (Directory.Exists(System.AppDomain.CurrentDomain.BaseDirectory + "/History/" + novel_name + "/" + chapter_name + ".txt"))
+            {
+                using (StreamReader sr = new StreamReader(System.AppDomain.CurrentDomain.BaseDirectory + "/History/" + novel_name + "/" + chapter_name + ".txt", Encoding.UTF8))
+                {
+                    String line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        chapter_comment += line;//我的文本框命名为txtList
+                    }
+                }
+            }
+            return chapter_comment;
+        }
+    }
+}
