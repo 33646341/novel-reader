92f7566feeeaf645f6f2f64e04bc3b8c14eb3b24
33646341 <1759220040@qq.com>
2021-06-13 10:10:16 +0800

    属性编辑器实现

--- WpfApp1/Window2.xaml.cs
+++ WpfApp1/Window2.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WpfApp1.Data;
 
 namespace WpfApp1
 {
@@ -22,6 +23,31 @@ namespace WpfApp1
         public Window2()
         {
             InitializeComponent();
+
+
+            DemoModel = new PropertyGridModel
+            {
+                String = "TestString",
+                Enum = Gender.Female,
+                Boolean = true,
+                Integer = 98,
+                VerticalAlignment = VerticalAlignment.Stretch
+            };
         }
+
+        public static readonly DependencyProperty DemoModelProperty = 
+            DependencyProperty.Register("DemoModel", typeof(PropertyGridModel), typeof(Window2), 
+                new PropertyMetadata(default(PropertyGridModel)));
+
+        public PropertyGridModel DemoModel
+        {
+            get => (PropertyGridModel)GetValue(DemoModelProperty);
+            set => SetValue(DemoModelProperty, value);
+        }
+
     }
+
+
+
+
 }
