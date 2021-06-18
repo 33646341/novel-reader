be7af70f89328f0f5262e1ce2249ed7268a4b418
2408584819@qq.com <2408584819@qq.com>
2021-06-18 20:26:11 +0800

    添加朗读

---
+++ ReadTool/ReadTool/chapter_reader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace ReadTool
+{
+    class chapter_reader
+    {
+        public SpeechSynthesizer speak = new SpeechSynthesizer();
+
+        public void speak_begin(string chapter)
+        {
+            speak.Volume = 100;
+
+            speak.Rate = -1;
+
+            if (chapter != "")
+            {
+                speak.SpeakAsync(chapter);
+            }
+        }
+
+        public void speak_puase()
+        {
+            speak.Pause();
+        }
+    }
+}
